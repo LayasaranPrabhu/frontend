4ccb47d1dd5b7b0e22b6137e9a2f88524fac3410
Layasaran <pvlayasaran@gmail.com>
2025-04-17 19:37:39 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Home from './components/Home'
-import Add from './components/add'
+import Add from './components/Add'
 
 const App = () => {
   return (
